d74b1427aa17e79cd5fe8c7805b2614dfb17704c
growthly-maker <growthlyinc@gmail.com>
2025-04-07 21:59:40 +0200

    Ajout du champ isVerified au modèle User pour la vérification d'email

--- models/User.js
+++ models/User.js
@@ -35,6 +35,10 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
